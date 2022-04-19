a9bc54c5b53d4430cda7de80f638498b88e450a6
Siyuan Zhu <102143985+zsyfrank@users.noreply.github.com>
2022-04-19 16:17:42 +0800

    Add files via upload

--- derivative.cpp
+++ derivative.cpp
@@ -18,22 +18,30 @@ int main()
 	printf("please input the function\n");
 	scanf("%s",function);
 	if(strcmp(function,"sinx")==0)
+	{
 		fx1=sin(x);
 		fx2=sin(x+1e-6);
 		tododerivative(fx1,fx2);
+	}
 	if(strcmp(function,"cosx")==0)
+	{
 		fx1=cos(x);
 		fx2=cos(x+1e-6);
 		tododerivative(fx1,fx2);
+	}
 	if(strcmp(function,"logx")==0)
+	{
 		fx1=log(x);
 		fx2=log(x+1e-6);
 		tododerivative(fx1,fx2);
+	}
 	char *s1="x^";//s1 is used to judge whether the function we input is a power function
 	if(strncmp(function,s1,2)==0)
+	{
 		exponent=atof(function);
 		printf("%lf",atof(function));
 		fx1=pow(x,exponent);
 		fx2=pow(x+1e-6,exponent);
+	}
 	return 0;
 }
