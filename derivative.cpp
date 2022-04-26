b1a74f4092011b2eece3e9e54837798f5ec36b54
Siyuan Zhu <102143985+zsyfrank@users.noreply.github.com>
2022-04-26 15:04:48 +0800

    Update derivative.cpp

--- derivative.cpp
+++ derivative.cpp
@@ -2,38 +2,25 @@
 #include<string.h>
 #include<math.h>
 #include<stdlib.h>
-void tododerivative(double fx1,double fx2)
+double myderivative(double x,char *function)
 {
-	double derivative;
-	derivative = (fx2-fx1)/1e-6;
-	printf("%lf",derivative);
-}
-int main()
-{
-	double x,fx1,fx2;//fx1,fx1 is used to do the derivative
+	double fx1,fx2;//fx1,fx1 is used to do the derivative
 	double exponent;
-	printf("the value of x is\n");
-	scanf("%lf",&x);
-	char function[] ="";//function[] is where we store the function we input
-	printf("please input the function\n");
-	scanf("%s",function);
+	double derivative;
 	if(strcmp(function,"sinx")==0)
 	{
 		fx1=sin(x);
 		fx2=sin(x+1e-6);
-		tododerivative(fx1,fx2);
 	}
 	if(strcmp(function,"cosx")==0)
 	{
 		fx1=cos(x);
 		fx2=cos(x+1e-6);
-		tododerivative(fx1,fx2);
 	}
 	if(strcmp(function,"logx")==0)
 	{
 		fx1=log(x);
 		fx2=log(x+1e-6);
-		tododerivative(fx1,fx2);
 	}
 	char *s1="x^";//s1 is used to judge whether the function we input is a power function
 	if(strncmp(function,s1,2)==0)
@@ -43,5 +30,18 @@ int main()
 		fx1=pow(x,exponent);
 		fx2=pow(x+1e-6,exponent);
 	}
+	derivative = (fx2-fx1)/1e-6;
+	return derivative;
+}
+int main()
+{
+	double x,derivative;
+	printf("the value of x is\n");
+	scanf("%lf",&x);
+	char function[] ="";//function[] is where we store the function we input
+	printf("please input the function\n");
+	scanf("%s",function);
+	derivative=myderivative(x,function);
+	printf("%lf",derivative);
 	return 0;
 }
