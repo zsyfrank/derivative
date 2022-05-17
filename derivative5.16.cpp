f3c136b5bbbf44690e179256959e08b3cb9c1452
Siyuan Zhu <102143985+zsyfrank@users.noreply.github.com>
2022-05-17 15:18:27 +0800

    Update derivative5.16.cpp

--- derivative5.16.cpp
+++ derivative5.16.cpp
@@ -7,12 +7,15 @@ double myderiv(double x, double step_size, double (*func)(double),double h,doubl
 {
 	cout<<fixed<<setprecision(10)<<"func("<<x<<")="<<func(x)<<std::endl;
 	int i;
-	double deriv1,deriv2;
+	double deriv1,deriv2,error,prec_improve=0;
 	for(i=0;i<100;i++)
 	{
 		deriv1=(func(x+step_size)-func(x-step_size))/(2*step_size);
 	    deriv2=(func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size);
 	    cout<<"when the step size is"<<step_size<<"the error is"<<deriv1-deriv2<<std::endl;
+		prec_improve=fabs((error - (deriv1-deriv2))/deriv2);//to calculate how much the precision has been improved
+		cout<<"the error has been improved by"<<prec_improve<<std::endl;
+		error=deriv1-deriv2;
 	    if(fabs(deriv1-deriv2)<=prec)
 	    {
 		    break;
@@ -41,12 +44,16 @@ int main()
 	cout<<"please input the max tries:"<<std::endl;
 	cin>>max_tries;
     result1=myderiv(x,step_size,&sin,h,prec,max_tries);
+	cout<<"for sinx"<<endl;
 	cout<<fixed<<setprecision(10)<<"result1="<<result1<<std::endl;
 	result2=myderiv(x,step_size,&cos,h,prec,max_tries); 
+	cout<<"for cosx"<<endl;
 	cout<<fixed<<setprecision(10)<<"result2="<<result2<<std::endl;
 	result3=myderiv(x,step_size,&exp,h,prec,max_tries);  
+	cout<<"for e^x"<<endl;
 	cout<<fixed<<setprecision(10)<<"result3="<<result3<<std::endl;
 	result4=myderiv(x,step_size,&log,h,prec,max_tries);  
+	cout<<"for lnx"<<endl;
 	cout<<fixed<<setprecision(10)<<"result4="<<result4<<std::endl;
     return 0;
 }
