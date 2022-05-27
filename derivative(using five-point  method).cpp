7190e218b2be427a895e456e192f50aa689a6d09
Siyuan Zhu <102143985+zsyfrank@users.noreply.github.com>
2022-05-27 14:27:16 +0800

    May 25th update
    apply dynamical choice on the step size for the numerical derivative based on the error estimate(five point method)

--- derivative(using five-point  method).cpp
+++ derivative(using five-point  method).cpp
@@ -3,6 +3,36 @@
 #include<stdio.h>
 #include<math.h>
 using namespace std;
+double derivative(double x,double (*func)(double),double prec,bool &flag)//guess the the proper step_size for target precision
+{
+	flag = 1;
+	double step_size = pow(prec,4);//estimate the step_size basing on precision
+	double deriv1 = (func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size);
+	step_size = step_size/2;
+	double deriv2 = (func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size);
+	double error = fabs(deriv1-deriv2);
+	if(error <= prec)
+	{
+		cout<<deriv1<<","<<deriv2<<","<<step_size<<std::endl;
+		flag = 0;
+		cout<<"it's a good guess,and the derivative is"<<deriv2<<std::endl;
+	}
+	else
+	{
+		cout<<"it's a guess is not good enough ,and the present derivative on STEP1 is"<<deriv2<<std::endl;
+		int i = 0;
+		while(error > prec)
+		{
+			step_size = step_size/2;
+			error = fabs(deriv2 - (func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size));
+			deriv2 = (func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size);
+			i++;
+		}
+		cout<<"it's on STEP"<<i<<" that the target precision is reached"<<",and the derivative is"<<deriv2<<std::endl;
+	}
+	return deriv2;
+}
+
 double myderiv(double x, double step_size, double (*func)(double),double h,double prec,double max_tries,bool &flag)// stepsize is divided by h every time
 {
 	cout<<fixed<<setprecision(10)<<"func("<<x<<")="<<func(x)<<std::endl;
@@ -14,23 +44,18 @@ double myderiv(double x, double step_size, double (*func)(double),double h,doubl
 	    deriv2=(func(x+2*step_size)+4*func(x+step_size)-func(x-2*step_size)-4*func(x-step_size))/(12*step_size);
 	    cout<<"when the step size is"<<step_size<<"the error is"<<deriv1-deriv2<<std::endl;
 		prec_improve=fabs((error - (deriv1-deriv2))/deriv2);//to calculate how much the precision has been improved
-	    cout<<"the error has been improved by"<<prec_improve<<std::endl;
+	 //   cout<<"the error has been improved by"<<prec_improve<<std::endl;
 		error=deriv1-deriv2;
 	    if(fabs(deriv1-deriv2)<=prec)
 	    {
 			flag = 0;
 		    break;
 	    }
-	    else if(fabs(deriv1-deriv2)>10*prec)
-		{
-			h=pow(10,max_tries);
-			step_size=step_size/h;
-		}
-		else 
-		    step_size=step_size/10;
+	    step_size=step_size/h;
 	}
 	return deriv1;
 }
+
 int main()
 {   
     double x,step_size,h,prec,max_tries;
@@ -38,43 +63,45 @@ int main()
     double result1,result2,result3,result4;
 	cout<<"please input the parameter:"<<std::endl;
     cin>>x;
-    cout<<"please input the initial step size:"<<std::endl;
-    cin>>step_size;
 	cout<<"please input the precision you need:"<<std::endl;
 	cin>>prec;
+	cout<<"please input the step size:"<<std::endl;
+	cin>>step_size;
+	cout<<"please input the multiple that stepsize is reduced every time:"<<std::endl;
+	cin>>h;
 	cout<<"please input the max tries:"<<std::endl;
 	cin>>max_tries;
 //sinx
 	cout<<"for sinx"<<endl;
-    result1=myderiv(x,step_size,&sin,h,prec,max_tries,flag);
-	cout<<fixed<<setprecision(10)<<"result1="<<result1<<std::endl;
-	if(flag)
-	{
-		cout<<"failured to get the aimed precision with maxtries"<<endl;
+    result1 = derivative(x,&sin,prec,flag);
+	if(flag)// geting derivative through comparing the two methods in case the guess failed
+	{	
+		result1=myderiv(x,step_size,&sin,h,prec,max_tries,flag);
 	}
+	cout<<fixed<<setprecision(10)<<"sinx'="<<result1<<std::endl;
 //cosx
 	cout<<"for cosx"<<endl;
-	result2=myderiv(x,step_size,&cos,h,prec,max_tries,flag); 
-	cout<<fixed<<setprecision(10)<<"result2="<<result2<<std::endl;
-	if(flag)
+	result2 = derivative(x,&cos,prec,flag);
+	if(flag)// geting derivative through comparing the two methods in case the guess failed
 	{
-		cout<<"failured to get the aimed precision with maxtries"<<endl;
+		result2=myderiv(x,step_size,&cos,h,prec,max_tries,flag);
 	}	
+	cout<<fixed<<setprecision(10)<<"cosx'="<<result2<<std::endl;
 //e^x
 	cout<<"for e^x"<<endl;
-	result3=myderiv(x,step_size,&exp,h,prec,max_tries,flag); 
-	cout<<fixed<<setprecision(10)<<"result3="<<result3<<std::endl;
-	if(flag)
+	result3=derivative(x,&exp,prec,flag);
+	if(flag)// geting derivative through comparing the two methods in case the guess failed
 	{
-		cout<<"failured to get the aimed precision with maxtries"<<endl;
-	}
+		result3=myderiv(x,step_size,&exp,h,prec,max_tries,flag); 
+	}	
+	cout<<fixed<<setprecision(10)<<"e^x'="<<result3<<std::endl;
 //lnx
 	cout<<"for lnx"<<endl;
-	result4=myderiv(x,step_size,&log,h,prec,max_tries,flag);  
-	cout<<fixed<<setprecision(10)<<"result4="<<result4<<std::endl;
-	if(flag)
+	result4=derivative(x,&log,prec,flag);
+	if(flag)// geting derivative through comparing the two methods in case the guess failed
 	{
-		cout<<"failured to get the aimed precision with maxtries"<<endl;
+		result4=myderiv(x,step_size,&log,h,prec,max_tries,flag);  
 	}
+	cout<<fixed<<setprecision(10)<<"lnx'="<<result4<<std::endl;
     return 0;
 }
